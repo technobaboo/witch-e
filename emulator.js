a95968e3df1919517964610b98aa8afb1feafec4
jsking <justinscottking@gmail.com>
2018-10-15 00:50:11 +0000

    Added a data object for organization

---
+++ emulator.js
@@ -0,0 +1,29 @@
+var data = {
+	log: {
+		text: "WITCH-E v. 2.0.0a",
+		lines: [
+			"WITCH-E v. 2.0.0a"
+		]
+	},
+	tapes: [
+		{
+			looped: true,
+			text: "",
+			lines: [
+				""
+			]
+		}
+	],
+	printer: {
+		text: "",
+		lines: [
+			""
+		]
+	},
+	stores: [],
+	mode: "single-step",
+	alarm: false,
+	finished: false
+};
+
+
